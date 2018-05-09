92ab29824337f8dbdc34c280e341bf0683c1725f
Timothy Clem <timothy.clem@gmail.com>
2018-05-09 13:44:46 -0700

    Little bit softer cursor behaviors

--- behaviors/cursor.js
+++ behaviors/cursor.js
@@ -6,20 +6,41 @@ class Cursor {
   }
 
   update(path, oldPos, newPos) {
-    let d = Math.random()
-
-    // Only act 3% of the time
-    if (d < 0.97) return
+    let now = new Date()
+    let state = this.paths[path] || {}
 
     if (newPos.row === oldPos.row - 1) {
-      global.furbies.playAction("ear point up, Up")
+      state.up = state.up || 0
+      state.up += 1
     } else if (newPos.row === oldPos.row + 1) {
-      global.furbies.playAction("down")
-    } else if (newPos.column === oldPos.column - 1) {
-      global.furbies.playAction("left")
-    } else if (newPos.column === oldPos.column + 1) {
-      global.furbies.playAction("right")
+      state.down = state.down || 0
+      state.down += 1
+    }
+
+    if (state.lastUpdateAt && now - state.lastUpdateAt > 30000) {
+      state.up = 0
+      state.down = 0
+    }
+
+    if (state.up > 20) {
+      state.up = 0
+      this.maybePlayAction("ear point up, Up")
+    }
+    if (state.down > 20) {
+      state.down = 0
+      this.maybePlayAction("down")
+    }
+
+    state.lastUpdateAt = new Date()
+    this.paths[path] = state
+  }
+
+  maybePlayAction(action) {
+    // Only act 50% of the time
+    if (Math.random() < 0.50) {
+      global.furbies.playAction(action)
     }
   }
 }
+
 module.exports = new Cursor()
