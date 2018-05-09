e476fca53eea58bc4145d328bbd99ba712048d0e
Timothy Clem <timothy.clem@gmail.com>
2018-05-09 14:34:53 -0700

    Dial in cursor a bit more

--- behaviors/cursor.js
+++ behaviors/cursor.js
@@ -3,17 +3,19 @@ const sample = require('../util/sample')
 class Cursor {
   constructor() {
     this.paths = {}
+    this.threshold = 30
   }
 
   update(path, oldPos, newPos) {
     let now = new Date()
     let state = this.paths[path] || {}
 
+    state.up = state.up || 0
+    state.down = state.down || 0
+
     if (newPos.row === oldPos.row - 1) {
-      state.up = state.up || 0
       state.up += 1
     } else if (newPos.row === oldPos.row + 1) {
-      state.down = state.down || 0
       state.down += 1
     }
 
@@ -22,11 +24,13 @@ class Cursor {
       state.down = 0
     }
 
-    if (state.up > 20) {
+    if (state.up > this.threshold) {
       state.up = 0
+      state.down = 0
       this.maybePlayAction("ear point up, Up")
     }
-    if (state.down > 20) {
+    if (state.down > this.threshold) {
+      state.up = 0
       state.down = 0
       this.maybePlayAction("down")
     }
